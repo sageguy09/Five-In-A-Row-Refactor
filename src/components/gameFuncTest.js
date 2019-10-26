4fe5478571e7c788366b23122233562d3b546629
Ryan Sage <ryansage09@gmail.com>
2019-10-25 23:15:42 -0400

    adding testing files for refactoring of game functioins

---
+++ src/components/gameFuncTest.js
@@ -0,0 +1,118 @@
+
+
+
+
+function initialState(ctx, state) {
+    let deckArray = []
+    for (let c = 0; c<100; c++){
+        deckArray.push(c)
+    }
+    let deck = ctx.random.Shuffle(deckArray)
+    return state || {
+        board: {
+            //deck: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            deck,
+            burn: [],
+            boardArray :  [
+                90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+                89, 42, 43, 44, 45, 46, 47, 48, 49, 64,
+                88, 41, 20, 21, 22, 23, 24, 25, 50, 65,
+                87, 40, 19, 6,   7,  8,  9, 26, 51, 66, 
+                86, 39, 18, 5,   0,  1, 10, 27, 52, 67,
+                85, 38, 17, 4,   3,  2, 11, 28, 53, 68,
+                84, 37, 16, 15, 14, 13, 12, 29, 54, 69,
+                83, 36, 35, 34, 33, 32, 31, 30, 55, 70,
+                82, 63, 62, 61, 60, 59, 58, 57, 56, 71,
+                81, 80, 79, 78, 77, 76, 75, 74, 73, 72
+              ]
+        },
+        players: {
+            0 : {hand: []},
+            1 : {hand: []}
+          },
+        cells: Array(100).fill(null),
+    }
+}
+
+
+const dealCards = (currentState, ctx) => {
+    for(let i=0; i< ctx.numPlayers; i++){ 
+    for(let j = 0; j < 4; j++){
+            let playerId = (i)
+            let currentPlayers = currentState.players
+            let selectedPlayer = currentState.players[playerId]
+            let boardId="board"
+            let currentBoard = currentState[boardId]
+            let deck = currentBoard.deck
+            let hand = selectedPlayer.hand
+            hand.push(deck.pop());
+            let player = {...selectedPlayer,  hand: hand};
+            let playersu = {...currentPlayers,  [playerId]: player}
+            let board = {...currentBoard, deck};
+            let state = {...currentState, players: playersu, [boardId]: board}
+        }  
+    }
+    return currentState
+}
+
+const drawCard = (currentState, ctx) => {
+    let playerId = ctx.currentPlayer
+    let currentPlayers = currentState.players
+    let currentPlayer = currentState.players[playerId]
+    let boardId = "board"
+    let currentBoard = currentState[boardId]
+    let deckIndex = currentBoard.deck.length - 1; 
+    let hand = ImmutableArray.append(currentPlayer.hand, currentBoard.deck[deckIndex]);
+    let deck = ImmutableArray.removeAt(currentBoard.deck, deckIndex);
+    let player = {...currentPlayer, hand: hand}
+    let playersu = {...currentPlayers, [playerId]: player }
+    //console.log(players)
+    let board = {...currentBoard, deck};
+    let state = {...currentState,  players: playersu, [boardId]: board}
+    return(state)
+}
+
+const playCard = (currentState, ctx, cardId) => {
+    let playerId = ctx.currentPlayer
+    let currentPlayers = currentState.players
+    let currentPlayer = currentState.players[playerId]
+    let boardId="board"
+    let currentBoard = currentState[boardId]
+    //find the card in hand, add hand.card to  burn
+    let handIndex = currentPlayer.hand.indexOf(cardId);
+    let burn = ImmutableArray.append(currentBoard.burn, currentPlayer.hand[handIndex])
+    //remove card from player hand. 
+    let hand = ImmutableArray.removeAt(currentPlayer.hand, handIndex)
+    //construct and return a new state object with changes.
+    let player = {...currentPlayer, hand: hand}
+    let playersu = {...currentPlayers, [playerId]: player }
+    let board = {...currentBoard, burn};
+    let state = {...currentState,  players: playersu, [boardId]: board}
+    //set game to play stage for player to play on a space
+    //ctx.events.setActivePlayers({player: 'play', moveLimit: 1})
+    return (state)
+}
+
+const playOnSpace = (currentState, ctx, id) => {
+    if (currentState.cells[id] !== null ) {
+        return INVALID_MOVE;
+    }
+    //fill cell with 0 or 1 depending the current player.
+    currentState.cells[id] = ctx.currentPlayer;
+    return currentState
+    //ctx.events.endTurn();
+    //console.log('post state: '+G.cells[id])
+    //console.log('postCTX: '+{ctx})
+}
+
+const ImmutableArray = {
+    append(arr, value) {
+        return [...arr, value];
+    },
+    removeAt(arr, index) {
+        return [...arr.slice(0, index), ...arr.slice(index + 1)];
+    },
+};
+
+export {initialState, dealCards, drawCard, playCard, playOnSpace}
+
---
+++ src/components/gametest.test.js
@@ -0,0 +1,57 @@
+import {initialState, dealCards, drawCard, playCard, playOnSpace,} from './gameFuncTest'
+
+import { isMainThread } from 'worker_threads';
+import { tsExternalModuleReference, exportAllDeclaration } from '@babel/types';
+
+const mockCtx = {
+    numPlayers: 2,
+    turn: 0,
+    currentPlayer: "0",
+    currentBoard: "1",
+    playOrder: ["0", "1"]
+};
+
+
+
+let mockState = {
+    board: {
+        deck: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        burn: [],
+    },
+    players: {
+        0 : {hand: []},
+        1 : {hand: []}
+      },
+        cells: Array(100).fill(null),        
+    }
+// test('drawing a card', () => {
+//     let state_1 = initialState(mockCtx, mockState)
+//     expect(state_1.players[0].hand.length).toEqual(0)
+//     let state_2 = drawCard(state_1, mockCtx)
+//     //console.log(state_2.players[0].hand)
+//     expect(state_2.players[0].hand.length).toEqual(1)
+// })
+
+// test('playing a card', () => {
+//     let state_1 = initialState(mockCtx, mockState)
+//     expect(state_1.players[0].hand.length).toEqual(0)
+//     let state_2 = drawCard(state_1, mockCtx);
+//     expect(state_2.players[0].hand.length).toEqual(1)
+//     let state_3 = playCard(state_2, mockCtx, 10)
+//     expect(state_3.players[0].hand.length).toEqual(0)
+// })
+
+test('dealing initial cards', () => {
+    let state_4 = initialState(mockCtx, mockState)
+    expect(state_4.players[0].hand.length).toEqual(0)
+    let state_5 = dealCards(mockState, mockCtx)    
+    expect(state_5.players[0].hand.length).toEqual(4)
+    expect(state_5.players[1].hand.length).toEqual(4)
+})
+
+test('playing card on space', () => {
+    let state_6 = initialState(mockCtx, mockState)
+    expect(state_6.cells[10]).toEqual(null)
+    let state_7 = playOnSpace(mockState, mockCtx, mockState.players[0].hand[1])
+    expect(state_7.cells[mockState.players[0].hand[1]]).toEqual(mockCtx.currentPlayer)
+})
