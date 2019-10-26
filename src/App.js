05765da47a31885e00feaf7b063e9c700d1bcc18
Ryan Sage <ryansage09@gmail.com>
2019-10-26 00:35:48 -0400

    moved old files to seperate directory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, {Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Home from './components/Home'
 import './App.css';
 import {Link} from 'react-router-dom'
 import FiveInARow from './components/gameLogic'
-import CreateGame from './components/createGame'
 class App extends Component {
 
 
