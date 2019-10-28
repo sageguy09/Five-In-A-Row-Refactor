d75bd24497a3f608577bda7d3e1633c573ae962c
Ryan Sage <ryansage09@gmail.com>
2019-10-28 17:57:10 -0400

    testing new method for heroku deployment

--- src/components/serverMultiplayer.js
+++ src/components/serverMultiplayer.js
@@ -4,12 +4,17 @@ import  FirGame  from './firGame';
 import FirBoard  from './firBoard';
 const hostname = window.location.hostname;
 //const gameUrl = `https://${process.env.REACT_APP_GAME_SERVER_URL}`
+const url = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port: '');
 const GAMEPORT = process.env.PORT || 8000
 const FirClient = Client({
     game: FirGame,
     board: FirBoard,
     debug: false,
-    multiplayer: {server: `${hostname}:${GAMEPORT}`}
+    
+    multiplayer: {
+        server: (process.env.NODE_ENV === 'production') ? `${url}` : `${window.location.hostname}:${GAMEPORT}`
+      },
+    //multiplayer: {server: `${hostname}:${GAMEPORT}`}
     //multiplayer: {server: `hidden-temple-14129.herokuapp.com:${443}`}
 })
 
