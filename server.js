6faa4928a39e62454d1e42810d1c84a6c988f35c
Ryan Sage <ryansage09@gmail.com>
2019-10-28 13:24:55 -0400

    initial KOA backend setupJ

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+import path from 'path';
+import serve from 'koa-static';
+import { historyApiFallback } from 'koa2-connect-history-api-fallback'
 import { Server } from 'boardgame.io/server';
 import FirGame from './src/components/firGame'
 
-const PORT = process.env.PORT || 8000;
+
+const { REACT_APP_SERVER_PORT } = process.env;
+const PORT = REACT_APP_SERVER_PORT || process.env.PORT || 8000;
 const server = Server({games: [FirGame]});
+
+const { app } = server;
+
+const root = path.join(__dirname, '../');
+
+app.use(
+    historyApiFallback({ index: 'index.html', whiteList: ['/api', '/games']})
+)
+
+app.use(serve(root))
+
 server.run(PORT, () => {
     console.log(`Serving at: http://localhost:${PORT}/`)
 })
