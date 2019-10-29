a315cc41dcfffe3585de6428fb3d79e3000fc142
Ryan Sage <ryansage09@gmail.com>
2019-10-29 13:44:52 -0400

    fixed player hand view to show the logged in player's hand.

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -5,12 +5,13 @@ import { default as FirGame } from './firGame';
 import './lobby.css' 
 
 FirGame.minPlayers = FirGame.maxPlayers = 2;
+FirGame.debug = true;
 
 const hostname = window.location.hostname;
 const url = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port: '');
 const GAMEPORT = process.env.PORT || 8000
 const importedGames = [
-    { game: FirGame, board: FirBoard }
+    { game: FirGame, board: FirBoard}
 ];
 
 const LobbyView = () => (
